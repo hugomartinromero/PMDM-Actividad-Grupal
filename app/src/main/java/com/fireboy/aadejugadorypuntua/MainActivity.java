60043a1528125c08c5a984d76a8e087d5c893911
Hugo <martin.romero.hugo@iescamas.es>
2024-10-15 15:22:47 +0200

    Actividad terminada

--- app/src/main/java/com/fireboy/aadejugadorypuntua/MainActivity.java
+++ app/src/main/java/com/fireboy/aadejugadorypuntua/MainActivity.java
@@ -6,6 +6,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
 import android.widget.Spinner;
@@ -14,20 +16,23 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup rgEquipos;
+    RadioButton rbRealMadrid, rbBarcelona, rbSevilla, rbBetis;
     EditText txtNombre;
     Button btnAniadir;
     Spinner spJugadores;
     TextView lblNombre, lblEstrellas, lblMediaEquipo;
     RatingBar ratingBar;
-    Set<String> jMadrid = new HashSet<>();
-    Set<String> jBarcelona = new HashSet<>();
-    Set<String> jSevilla = new HashSet<>();
-    Set<String> jBetis = new HashSet<>();
+    LinearLayout estrellasJugador;
+
+    Map<String, Float> jMadrid = new HashMap<>();
+    Map<String, Float> jBarcelona = new HashMap<>();
+    Map<String, Float> jSevilla = new HashMap<>();
+    Map<String, Float> jBetis = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rgEquipos = findViewById(R.id.rgEquipos);
+        rbRealMadrid = findViewById(R.id.rbRealMadrid);
+        rbBarcelona = findViewById(R.id.rbBarcelona);
+        rbSevilla = findViewById(R.id.rbSevilla);
+        rbBetis = findViewById(R.id.rbBetis);
         txtNombre = findViewById(R.id.txtNombre);
         btnAniadir = findViewById(R.id.btnAniadir);
         spJugadores = findViewById(R.id.spJugadores);
@@ -42,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         lblEstrellas = findViewById(R.id.lblEstrellas);
         lblMediaEquipo = findViewById(R.id.lblMediaEquipo);
         ratingBar = findViewById(R.id.ratingBar);
+        estrellasJugador = findViewById(R.id.estrellasJugador);
 
         rgEquipos.setOnCheckedChangeListener((radioGroup, i) -> cambiarLista());
         btnAniadir.setOnClickListener(v -> aniadirJugador());
@@ -52,71 +62,93 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {}
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
         });
-        ratingBar.setOnRatingBarChangeListener((ratingBar, v, b) -> darValor());
-    }
-
-    private void mostrarJugador() {
-        lblNombre.setText(spJugadores.getSelectedItem().toString());
+        ratingBar.setOnRatingBarChangeListener((ratingBar, v, b) -> {
+            darValor();
+            mostrarMediaEquipo();
+        });
     }
 
     private void cambiarLista() {
-        ArrayAdapter<String> adapter = null;
-        ArrayList<String> array = null;
+        ArrayList<String> array = new ArrayList<>(getJugadoresEquipo().keySet());
 
-        switch (verificarEquipo()) {
-            case 1:
-                array = new ArrayList<>(jMadrid);
-            break;
-            case 2:
-                array = new ArrayList<>(jBarcelona);
-                break;
-            case 3:
-                array = new ArrayList<>(jSevilla);
-                break;
-            case 4:
-                array = new ArrayList<>(jBetis);
-                break;
+        if (array != null) {
+            spJugadores.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, array));
+
+            if (spJugadores.getSelectedItem() != null) {
+                spJugadores.setVisibility(View.VISIBLE);
+                estrellasJugador.setVisibility(View.VISIBLE);
+            } else {
+                spJugadores.setVisibility(View.INVISIBLE);
+                estrellasJugador.setVisibility(View.INVISIBLE);
+            }
         }
-
-        spJugadores.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, array));
     }
 
     private void aniadirJugador() {
-        switch (verificarEquipo()) {
-            case 1:
-                jMadrid.add(txtNombre.getText().toString());
-                break;
-            case 2:
-                jBarcelona.add(txtNombre.getText().toString());
-                break;
-            case 3:
-                jSevilla.add(txtNombre.getText().toString());
-                break;
-            case 4:
-                jBetis.add(txtNombre.getText().toString());
-                break;
-        }
+        String nombreJugador = txtNombre.getText().toString();
 
-        cambiarLista();
+        if (!nombreJugador.isEmpty()) {
+            getJugadoresEquipo().put(nombreJugador, ratingBar.getRating());
+            cambiarLista();
+        }
+    }
+
+    private void mostrarJugador() {
+        String jugadorSeleccionado = (String) spJugadores.getSelectedItem();
+        if (jugadorSeleccionado != null) {
+            lblNombre.setText(jugadorSeleccionado);
+            ratingBar.setRating(getJugadoresEquipo().get(jugadorSeleccionado));
+        }
     }
 
     private void darValor() {
-        int valor = (int) ratingBar.getRating();
-        lblEstrellas.setText(valor);
+        float estrellas = ratingBar.getRating();
+
+        getJugadoresEquipo().put(txtNombre.getText().toString(), estrellas);
+        lblEstrellas.setText(String.valueOf(estrellas));
     }
 
-    private int verificarEquipo() {
-        if (rgEquipos.getCheckedRadioButtonId() == R.id.rbRealMadrid)  {
-            return 1;
-        } else if (rgEquipos.getCheckedRadioButtonId() == R.id.rbBarcelona)  {
-            return 2;
-        } else if (rgEquipos.getCheckedRadioButtonId() == R.id.rbSevilla)  {
-            return 3;
-        } else if (rgEquipos.getCheckedRadioButtonId() == R.id.rbBetis)  {
-            return 4;
+    private void mostrarMediaEquipo() {
+        Map<String, Float> equipo = getJugadoresEquipo();
+
+        String nombreEquipo = getNombreEquipo();
+        float media = 0;
+
+        for (Float valor : equipo.values()) {
+            media += valor;
         }
-        return 0;
+
+        media /= equipo.size();
+
+        lblMediaEquipo.setText(String.format("El equipo %s tiene una media de %.2f", nombreEquipo, media));
+    }
+
+    private String getNombreEquipo() {
+        if (rgEquipos.getCheckedRadioButtonId() == R.id.rbRealMadrid) {
+            return rbRealMadrid.getText().toString();
+        } else if (rgEquipos.getCheckedRadioButtonId() == R.id.rbBarcelona) {
+            return rbBarcelona.getText().toString();
+        } else if (rgEquipos.getCheckedRadioButtonId() == R.id.rbSevilla) {
+            return rbSevilla.getText().toString();
+        } else if (rgEquipos.getCheckedRadioButtonId() == R.id.rbBetis) {
+            return rbBetis.getText().toString();
+        }
+        return "";
+    }
+
+    private Map<String, Float> getJugadoresEquipo() {
+        if (getNombreEquipo().equals("Real Madrid")) {
+            return jMadrid;
+        } else if (getNombreEquipo().equals("Barcelona")) {
+            return jBarcelona;
+        } else if (getNombreEquipo().equals("Sevilla FC")) {
+            return jSevilla;
+        } else if (getNombreEquipo().equals("Real Betis")) {
+            return jBetis;
+        }
+        return null;
     }
 }
